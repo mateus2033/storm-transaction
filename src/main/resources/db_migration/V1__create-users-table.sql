073d5c8621c06d916b0ffd3d65ccbe408695d94b
mateus2033 <mateus2033@outlook.com>
2024-06-06 22:16:25 -0300

    testando migrations

--- src/main/resources/db_migration/V1__create-users-table.sql
+++ src/main/resources/db_migration/V1__create-users-table.sql
@@ -1,2 +1,6 @@
-INSERT INTO users (id, cellphone, cpf, data_birth, email, image, last_name, name, password)VALUES
-('550e8400-e29b-41d4-a716-446655440000','+1234567890','123.456.789-01','1990-01-15 10:30:00','exemplo@email.com','image1.jpg','Belmont','Sara','123456');
+INSERT INTO users (id, name, last_name, cpf, data_birth, cellphone, image, email, password) VALUES
+('6802bcf9-bc2f-4402-b2ab-6dc9582077a9', 'Mateus', 'Pereira', '672.303.630-12', '2024-01-01', '27 99977-9999', 'image.jpg', 'mateus2033@gmail.com', '12345678'),
+('8692cd05-2dc1-4c36-b075-94e4879f6512', 'Ana', 'Silva', '123.456.789-01', '1990-02-15', '27 98888-8888', 'ana.jpg', 'ana.silva@example.com', 'senhaAna123'),
+('d3323d31-4813-4849-ab28-759668869d0c','Carlos', 'Souza', '987.654.321-00', '1985-06-30', '27 97777-7777', 'carlos.jpg', 'carlos.souza@example.com', 'senhaCarlos123'),
+('462a4444-c641-4a2d-ba94-e193b6a1e17f','Maria', 'Oliveira', '456.789.123-45', '1975-11-20', '27 96666-6666', 'maria.jpg', 'maria.oliveira@example.com', 'senhaMaria123'),
+('76969b7d-80b9-456f-b393-bfdb0a49576c','João', 'Costa', '789.123.456-78', '2000-04-25', '27 95555-5555', 'joao.jpg', 'joao.costa@example.com', 'senhaJoao123');
